f22961b52aa76d519e6e5a6b9a6500f7eb3b2111
ollie <ojs.anorak2000@gmail.com>
2023-03-18 00:51:47 +0900

    CRUD methods added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # REST API built in Flask
-#### Description:
+#### Description: REST API with CRUD methods for database that store products to potentially buy.
+
+###### TODO:
+* Add Patch
+* Add more tables related with foreign keys
+* Separate models and schema into separate file from app logic
+* Add front-end
--- app.py
+++ app.py
@@ -17,10 +17,10 @@ ma = Marshmallow(app)
 # Product Model
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    location = db.Column(db.String(100))
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
 
     def __init__(self, name, description, price, location):
         self.name = name
@@ -36,6 +36,61 @@ class ProductSchema(ma.Schema):
 # Init schema
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
+
+# Add Product
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    location = request.json['location']
+
+    new_product = Product(name, description, price, location)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+# Get All Products
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    return products_schema.jsonify(all_products)
+
+# Get a Product
+@app.route('/product/<int:id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+# Update Product Completely
+@app.route('/product/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    location = request.json['location']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.location = location
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+# Delete a Product
+@app.route('/product/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
 
 # Run server
 if __name__ == '__main__':
