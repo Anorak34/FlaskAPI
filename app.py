ebb2b4d7b5220da62f45174dbfeab7d072b5fdc8
ollie <ojs.anorak2000@gmail.com>
2023-03-17 11:36:30 +0900

    db setup

--- app.py
+++ app.py
@@ -5,6 +5,37 @@ import os
 
 # Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+
+# Product Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    location = db.Column(db.String(100))
+
+    def __init__(self, name, description, price, location):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.location = location
+
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'location')
+
+# Init schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
 # Run server
 if __name__ == '__main__':
